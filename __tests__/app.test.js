d143656f7fee9d2be3a85023bda2ba265fd59099
Mariah <mariahschock@gmail.com>
2022-08-10 15:57:42 -0700

    set up data, test, controller, models, services, and app.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,27 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('should login and redirect users to github dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'git-this',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
